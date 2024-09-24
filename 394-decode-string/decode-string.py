8e87644d180e516b3cdf12359b16fab0a215f59f
Sruthi Sivasankar <113873706+SruthiSivasankar@users.noreply.github.com>
2024-09-24 05:39:52 +0530

    Time: 28 ms (91.07%) | Memory: 16.4 MB (95.26%) - LeetSync

--- 394-decode-string/decode-string.py
+++ 394-decode-string/decode-string.py
@@ -6,21 +6,15 @@ class Solution:
         
         for char in s:
             if char.isdigit():
-                # Build the current number (could be more than one digit)
                 current_num = current_num * 10 + int(char)
             elif char == '[':
-                # Push the current number and string onto the stack
                 stack.append((current_string, current_num))
-                # Reset for the new segment
                 current_string = ""
                 current_num = 0
             elif char == ']':
-                # Pop the last string and number
                 last_string, num = stack.pop()
-                # Repeat the current string num times and append to the last string
                 current_string = last_string + current_string * num
             else:
-                # It's a letter, append to the current string
                 current_string += char
         
         return current_string
